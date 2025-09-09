9e5eb5fc5953c30ed03e346592ad92de1779dc84
emdevelopa <gyimahsunday@gmail.com>
2025-09-09 21:16:27 +0100

    login page and sign up page added

--- src/pages/signup/page.jsx
+++ src/pages/signup/page.jsx
@@ -23,7 +23,7 @@ export default function Register() {
   };
 
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center p-4">
+    <div className="min-h-screen bg-[#161616] flex items-center justify-center p-4">
       <div className="w-full max-w-md bg-gray-900 p-6 rounded-2xl shadow-lg">
         <h2 className="text-2xl font-bold text-white mb-6 text-center">
           Create an Account
